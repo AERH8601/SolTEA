135cc4603ff74572bfec24eb11431ffe58fee1d3
hugo.quintero <hugo.quintero@udea.edu.co>
2024-09-12 17:07:20 -0500

    Corrección NavigationCard en Index

--- client/soltea_f/pages/index.tsx
+++ client/soltea_f/pages/index.tsx
@@ -1,15 +1,14 @@
 import { NavigationCard } from "@/components/NavigationCard"
 
 const Home = () => {
-  return (
-  
+  return (  
       <main>
           <h1>INICIO SOLTE PROJECT</h1>          
           <div>
-            <NavigationCard title= 'services' description= 'Oferta de nuestros servicios' />
-            <NavigationCard title= 'projects' description= 'nuestros proyectos' />
-            <NavigationCard title= 'blogs' description= 'Información relacionada a la sostenibilidad' />
-            <NavigationCard title= 'contact' description= 'Como contactarnos' />
+            <NavigationCard title= 'services' description= 'Oferta de nuestros servicios' href="href"/>
+            <NavigationCard title= 'projects' description= 'nuestros proyectos' href="href" />
+            <NavigationCard title= 'blogs' description= 'Información relacionada a la sostenibilidad' href="href" />
+            <NavigationCard title= 'contact' description= 'Como contactarnos' href="href" />
           </div>
       </main>
         );
